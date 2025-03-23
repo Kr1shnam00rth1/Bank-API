cd86568515d9132b1fd8175518fdaa4bd58528a3
Kr1shnam00rth1 <plkrishnamoorthi2004@gmail.com>
2025-03-23 13:24:40 +0530

    Update cashierRouter.js

--- router/cashierRouter.js
+++ router/cashierRouter.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const { authenticateToken } = require('../middleware/jwtMiddleware');
 const { checkCashierAccess } = require('../middleware/accessControlMiddleware');
-const { cashierLogin, cashierDeposit ,cashierWithdrawal, cashierUserAccountInfo } = require('../controller/cashierController');
+const { cashierLogin, cashierDeposit ,cashierWithdrawal, cashierUserAccountInfo, cashierChangePassword} = require('../controller/cashierController');
 
 router.post("/login", cashierLogin);
 router.post("/deposit", authenticateToken, checkCashierAccess, cashierDeposit);
 router.post("/withdrawal", authenticateToken, checkCashierAccess, cashierWithdrawal);
 router.post("/userAccountInfo", authenticateToken, checkCashierAccess, cashierUserAccountInfo);
+router.post("/changePassword", authenticateToken, checkCashierAccess, cashierChangePassword);
 
 module.exports = router; 
